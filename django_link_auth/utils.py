3ae8d14297f0a8e284e520656c6d81549bab46ab
Pierre-Alexandre Bardina <pa.bardina@gmail.com>
2014-07-15 16:36:38 +0200

    fix bug

--- django_link_auth/utils.py
+++ django_link_auth/utils.py
@@ -1,10 +1,8 @@
 import datetime
-import pytz
 
 from django.contrib.sites.models import Site
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
-from django.utils.translation import ugettext_lazy as _
 from django.conf import settings
 
 from django_link_auth.settings import \
@@ -12,13 +10,7 @@ from django_link_auth.settings import \
     LINK_AUTH_EMAIL_TITLE, \
     LINK_AUTH_EMAIL_FROM
 
-STRIP_TIMEZONE = settings.DATABASE_ENGINE == 'mysql'
-
-if STRIP_TIMEZONE:
-    utcnow = lambda: datetime.datetime.utcnow()
-else:
-    utcnow = lambda: pytz.utc.localize(datetime.datetime.utcnow())
-
+utcnow = lambda: datetime.datetime.utcnow()
 
 def send_link_by_email(sender, email, hash, **kwargs):
     site = Site.objects.get_current()
