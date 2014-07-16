fbbda95d887be8e841c160d6b50bbcb08617aa61
P-A Bardina <pa.bardina@gmail.com>
2014-07-16 13:44:50 +0200

    add method in utils.py

--- django_link_auth/utils.py
+++ django_link_auth/utils.py
@@ -1,29 +1,23 @@
+import hashlib
+import time
 import datetime
-from django.contrib.sites.models import Site
-from django.core.mail import send_mail
-from django.template.loader import render_to_string
-from django_link_auth.settings import \
-    AUTH_LINK_LIFETIME, \
-    LINK_AUTH_EMAIL_TITLE, \
-    LINK_AUTH_EMAIL_FROM
+#from django_link_auth.models import Hash
 
 utcnow = lambda: datetime.datetime.utcnow()
+#
+#
+#def generate_link(**kwargs):
+#    email = kwargs.get('email', None)
+#    location = kwargs.get('location', '/')
+#    key = kwargs.get('key', None)
+#
+#    if email and location:
+#        hash = hashlib.md5(email + key + str(time.time())).hexdigest()
+#        Hash(email=email, hash=hash, next=location).save()
 
 
-def send_link_by_email(sender, email, hash, **kwargs):
-    site = Site.objects.get_current()
-
-    params = dict(
-        domain=site, site_name = site.name, hash = hash,
-        expire_at=utcnow() + \
-                    datetime.timedelta(0, AUTH_LINK_LIFETIME)
-    )
-
-    email_body = render_to_string('email/auth_link.html', params)
-
-    send_mail(LINK_AUTH_EMAIL_TITLE % params,
-              email_body,
-              LINK_AUTH_EMAIL_FROM % params,
-              (email,)
-    )
-
+#def delete_hash(hash):
+#    try:
+#        hash = Hash.valid.get(hash=hash)
+#    except Hash.DoesNotExist:
+#        return None
