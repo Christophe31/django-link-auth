62fba9448af6afc6c1400a5c9ac2dbf137f5d367
Pierre-Alexandre Bardina <pa.bardina@gmail.com>
2014-07-17 11:37:25 +0200

    gitignore and util function

--- django_link_auth/utils.py
+++ django_link_auth/utils.py
@@ -1,23 +1,24 @@
 import hashlib
 import time
 import datetime
-#from django_link_auth.models import Hash
+from django_link_auth.models import Hash
 
 utcnow = lambda: datetime.datetime.utcnow()
-#
-#
-#def generate_link(**kwargs):
-#    email = kwargs.get('email', None)
-#    location = kwargs.get('location', '/')
-#    key = kwargs.get('key', None)
-#
-#    if email and location:
-#        hash = hashlib.md5(email + key + str(time.time())).hexdigest()
-#        Hash(email=email, hash=hash, next=location).save()
 
 
-#def delete_hash(hash):
-#    try:
-#        hash = Hash.valid.get(hash=hash)
-#    except Hash.DoesNotExist:
-#        return None
+def generate_link(**kwargs):
+   email = kwargs.get('email', None)
+   location = kwargs.get('location', '/')
+   key = kwargs.get('key', None)
+
+   if email and location:
+       hash = hashlib.md5(email + key + str(time.time())).hexdigest()
+       Hash(email=email, hash=hash, next=location).save()
+       return True
+
+
+def delete_hash(hash):
+   try:
+       hash = Hash.valid.get(hash=hash)
+   except Hash.DoesNotExist:
+       return None
